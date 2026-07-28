78a986590fa8f7e5cd4a4795ab7e0269f9170a3a
davidkovach-fuentes <davidkovach-fuentes2027@u.northwestern.edu>
2026-07-28 09:35:19 -0400

    bad commit

--- Formula/rt.rb
+++ Formula/rt.rb
@@ -3,15 +3,9 @@ class Rt < Formula
   homepage 'https://github.com/atlas-brown/rt'
   version '0.1.0'
   license :cannot_represent
-<<<<<<< HEAD
   # TODO: replace fields with atlas/rt
   url "https://github.com/davidkovach-fuentes/rt/releases/download/v#{version}/rt-#{version}.tar.gz"
   sha256 'sha256:3e70ed713350a5ca7691ebf441b1f5abd15f3be4a2cf1506f682cdf01d63b38e'
-=======
-  url "https://github.com/atlas-brown/rt/releases/download/v#{version}/rt-#{version}.tar.gz"
-  # curl -sL "https://github.com/atlas-brown/rt/releases/download/v0.1.0/rt-0.1.0.tar.gz" | shasum -a 256
-  sha256 ''
->>>>>>> e08b51142b7dc993d0da9dd3c493c648b6742ac6
 
   def install
     bin.install 'scripts/run-in-container.sh' => 'rt'
