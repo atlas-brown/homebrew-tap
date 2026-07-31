60db353dc134923abd5799753897b8475cb0ebbc
davidkovach-fuentes <davidkovach-fuentes2027@u.northwestern.edu>
2026-07-31 14:24:09 -0400

    use double quotes for brew --only-tap-syntax

--- Formula/rt.rb
+++ Formula/rt.rb
@@ -1,18 +1,17 @@
 class Rt < Formula
-  desc 'Overlay type system for Unix shell pipelines'
-  homepage 'https://github.com/atlas-brown/rt'
-  version '0.1.0'
-  # TODO: replace fields with atlas/rt
-  url 'https://github.com/davidkovach-fuentes/rt/archive/refs/tags/v0.1.0.tar.gz'
-  # url 'https://github.com/atlas-group/rt/archive/refs/tags/v0.1.0.tar.gz'
-  sha256 '1d93946042dd1b50969e8862897d633e939f630c5ef7fa33c44e2f3ee238d28b'
+  desc "Overlay type system for Unix shell pipelines"
+  homepage "https://github.com/atlas-brown/rt"
+  # TODO: replace fields with atlas-brown/rt
+  url "https://github.com/davidkovach-fuentes/rt/archive/refs/tags/v0.1.0.tar.gz"
+  # url "https://github.com/atlas-group/rt/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "1d93946042dd1b50969e8862897d633e939f630c5ef7fa33c44e2f3ee238d28b"
   license :cannot_represent
 
-  depends_on 'docker' => :test
+  depends_on "docker" => :test
 
   def install
-    bin.install 'scripts/run-in-container.sh' => 'rt'
-    bin.install_symlink 'rt' => 'rti'
+    bin.install "scripts/run-in-container.sh" => "rt"
+    bin.install_symlink "rt" => "rti"
   end
 
   def caveats
@@ -25,8 +24,8 @@ class Rt < Formula
   end
 
   test do
-    assert_predicate bin / 'rt', :executable?
-    assert_predicate bin / 'rti', :executable?
-    assert_match 'docker', File.read(bin / 'rt')
+    assert_predicate bin/"rt", :executable?
+    assert_predicate bin/"rti", :executable?
+    assert_match "docker", File.read(bin/"rt")
   end
 end
